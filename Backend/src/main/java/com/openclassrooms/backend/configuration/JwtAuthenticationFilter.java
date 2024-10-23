3e72de54909557f0d2382c4bcb43a120e3816c0f
miere221 <william.miere@etu.unicaen.fr>
2024-10-23 15:22:26 +0200

    tentative modification token

--- Backend/src/main/java/com/openclassrooms/backend/configuration/JwtAuthenticationFilter.java
+++ Backend/src/main/java/com/openclassrooms/backend/configuration/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -81,7 +82,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             // Allow the request.
             filterChain.doFilter(request, response);
-        } catch (Exception exception) {
+        } catch (ServletException | IOException | UsernameNotFoundException exception) {
             handlerExceptionResolver.resolveException(request, response, null, exception);
         }
     }
